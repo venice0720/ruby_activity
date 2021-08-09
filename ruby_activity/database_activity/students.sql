957d02dab286aeb6c85ad5d001d7642c05d7ec2b
venice0720 <vincentluague07@gmail.com>
2021-08-10 02:38:49 +0800

    Pushing database_activity folder with students.sql

---
+++ ruby_activity/database_activity/students.sql
@@ -0,0 +1,20 @@
+CREATE TABLE students(id integer PRIMARY KEY, first_name VARCHAR (100),middle_name VARCHAR (100),last_name VARCHAR (100),age INTEGER,location VARCHAR(100));
+
+SELECT * from students;
+
+INSERT into students (id, first_name,last_name, age, location) VALUES (1, 'Juan', 'Cruz', 18, 'Manila');
+
+INSERT into students (id, first_name,last_name, age, location) VALUES (2, 'Anne', 'Wall', 20, 'Manila');
+
+INSERT into students (id, first_name,last_name, age, location) VALUES (3, 'Theresa', 'Joseph', 21, 'Manila');
+
+INSERT into students (id, first_name,last_name, age, location) VALUES (4, 'Isaac', 'Gray', 19, 'Laguna');
+
+INSERT into students (id, first_name,last_name, age, location) VALUES (5, 'Zack', 'Matthews', 22, 'Marikina');
+
+INSERT into students (id, first_name,last_name, age, location) VALUES (6, 'Finn', 'Lam', 25, 'Manila');
+
+
+UPDATE students set first_name = 'Ivan', middle_name = 'Ingram', last_name = 'Howard', age = 25, location = 'Bulacan' where id = 1;
+
+DELETE FROM students where id = 6;
