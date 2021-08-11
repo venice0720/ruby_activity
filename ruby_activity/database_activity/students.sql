79588c4d8362aa648d35858ab42d3301bd1af669
venice0720 <vincentluague07@gmail.com>
2021-08-12 01:18:33 +0800

    Updating students.sql

--- ruby_activity/database_activity/students.sql
+++ ruby_activity/database_activity/students.sql
@@ -20,7 +20,6 @@ UPDATE students set first_name = 'Ivan', middle_name = 'Ingram', last_name = 'Ho
 DELETE FROM students where id = 6;
 
 -- New activity
-
 SELECT COUNT(id) FROM students;
 
 SELECT * from students WHERE location = 'Manila';
@@ -28,3 +27,24 @@ SELECT * from students WHERE location = 'Manila';
 SELECT AVG(age) FROM students;
 
 SELECT * from students ORDER BY age desc;
+
+
+-- New Activity (JOIN)
+
+CREATE TABLE classrooms(id integer PRIMARY KEY, section VARCHAR (3),student_id integer, FOREIGN KEY (student_id) REFERENCES students (id));
+
+INSERT into classrooms (id, student_id, section) VALUES (1, 1, 'A');
+INSERT into classrooms (id, student_id, section) VALUES (2, 2, 'A');
+INSERT into classrooms (id, student_id, section) VALUES (3, 3, 'B');
+INSERT into classrooms (id, student_id, section) VALUES (4, 4, 'C');
+INSERT into classrooms (id, student_id, section) VALUES (5, 5, 'B');
+INSERT into classrooms (id, student_id, section) VALUES (6, 6, 'A');
+INSERT into classrooms (id, student_id, section) VALUES (7, 7, 'C');
+INSERT into classrooms (id, student_id, section) VALUES (8, 8, 'B');
+INSERT into classrooms (id, student_id, section) VALUES (9, 9, 'B');
+INSERT into classrooms (id, student_id, section) VALUES (10, 10, 'C');
+
+SELECT * FROM students s INNER JOIN classrooms c ON c.student_id=s.id;
+SELECT * FROM students s LEFT JOIN classrooms c ON c.student_id=s.id;
+SELECT * FROM classrooms c RIGHT JOIN students s ON c.student_id=s.id;
+SELECT * FROM classrooms c FULL JOIN students s ON c.student_id=s.id;
