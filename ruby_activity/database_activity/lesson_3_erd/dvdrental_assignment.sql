b73cb85c81db8d70513310ce1e2d40f4d5cfbe50
venice0720 <vincentluague07@gmail.com>
2021-08-17 00:41:35 +0800

    Assignment Done ERD and DVDRENTAL

--- ruby_activity/database_activity/lesson_3_erd/dvdrental_assignment.sql
+++ ruby_activity/database_activity/lesson_3_erd/dvdrental_assignment.sql
@@ -4,16 +4,16 @@ SELECT COUNT(DISTINCT inventory_id) as unique_inventory_rented,COUNT(*) as total
 -- 2. 
 
 SELECT film.title as film_title, film.release_year as release_year, film.rating as rating, film.rating as rating, 
-film.rating as rating, actor.first_name||' '||actor.last_name as actor_full_name FROM actor inner join film_actor
-on actor.actor_id = film_actor.actor_id inner join film on film_actor.film_id = film.film_id where actor.first_name = 'Dan' 
-and actor.last_name = 'Torn' or actor.first_name = 'Dan' and actor.last_name = 'Streep' order by film_title;
+film.rating as rating, actor.first_name||' '||actor.last_name as actor_full_name FROM actor INNER JOIN film_actor
+on actor.actor_id = film_actor.actor_id INNER JOIN film on film_actor.film_id = film.film_id where actor.first_name = 'Dan' 
+and actor.last_name = 'Torn' or actor.first_name = 'Dan' and actor.last_name = 'Streep' ORDER BY film_title;
   
 -- 3. 
 
 SELECT actor.first_name||' '||actor.last_name as actor_full_name, film.title as film_title, category.name as category_name
-FROM actor inner join film_actor on actor.actor_id = film_actor.actor_id inner join film on film_actor.film_id = film.film_id
-inner join film_category on film.film_id = film_category.film_id inner join category on film_category.category_id = category.category_id
-where actor.last_name like 'D%' and category.name = 'Comedy' order by actor_full_name;
+FROM actor INNER JOIN film_actor on actor.actor_id = film_actor.actor_id INNER JOIN film on film_actor.film_id = film.film_id
+INNER JOIN film_category on film.film_id = film_category.film_id INNER JOIN category on film_category.category_id = category.category_id
+where actor.last_name like 'D%' and category.name = 'Comedy' ORDER BY actor_full_name;
   
 -- 4.  
 SELECT staff_list.name as staff_full_name, count(staff_list.country) as customer_count FROM staff_list JOIN customer_list on staff_list.country = customer_list.country
@@ -28,7 +28,7 @@ ORDER BY film_count desc;
 -- 6. 
 
 SELECT staff_list.country, concat(actor.first_name, ' ' , actor.last_name) as actor_full_name, RANK () over
-( partition by staff_list.country order by count(inventory.film_id) desc) as actor_rank, count(inventory.film_id) as rent_count 
+( partition by staff_list.country ORDER BY count(inventory.film_id) desc) as actor_rank, count(inventory.film_id) as rent_count 
 FROM rental JOIN inventory on rental.inventory_id = inventory.inventory_id JOIN film on film.film_id = inventory.film_id JOIN film_actor
 on film.film_id = film_actor.film_id JOIN actor on film_actor.actor_id = actor.actor_id JOIN staff_list on staff_list.id= rental.staff_id
 GROUP BY staff_list.country, actor_full_name ORDER BY actor_rank asc;
